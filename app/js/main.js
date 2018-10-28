25148d4e70e0cec2833bccf1f1d3551533ecd701
Nadal Soler <nadalsoler@gmail.com>
2018-10-28 20:49:11 +0100

    feat: hamburger menu for small devices

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// The document ready event executes already when the HTML-Document is loaded //
+// and the DOM is ready, even if all the graphics haven't loaded yet.         //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+const _onLoadListener = function () {
+  /**
+   * Hamburger menu
+   *
+   * 1. Toggle `header-nav` visibility.
+   * 2. Toggle `header-hamburger-open` and `header-hamburger-close` icons.
+   */
+  (function () {
+    var headerHamburger = document.querySelectorAll('#header-hamburger'),
+        headerHamburgerOpen = document.querySelectorAll('#header-hamburger-open'),
+        headerHamburgerClose = document.querySelectorAll('#header-hamburger-close'),
+        headerNav = document.querySelectorAll('#header-nav');
+
+    headerHamburger.forEach(_element => _element.addEventListener('click', function (event) {
+      event.preventDefault();
+      headerNav.forEach(_element2 => _element2.classList.toggle('is-small-hidden')); // 1
+
+      headerHamburgerOpen.forEach(_element3 => _element3.classList.toggle('is-hidden')); // 2
+      headerHamburgerClose.forEach(_element4 => _element4.classList.toggle('is-hidden')); // 2
+    }));
+  })();
+};
+
+if (document.readyState !== 'loading') {
+  _onLoadListener();
+} else {
+  document.addEventListener('DOMContentLoaded', _onLoadListener);
+}
